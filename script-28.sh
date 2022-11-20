40eec123c069052b63307c8234a7b0ab0cc5e3e3
Tsubasa <scientistjishan@gmail.com>
2022-11-20 15:50:08 +0530

    Added tree view, permission change etc.

--- script-28.sh
+++ script-28.sh
@@ -16,9 +16,26 @@ echo -e "Checking...\n"
 for name in $@; do
     if [[ -d $name ]]; then
         echo "$name is a directory."
+        tree $name
+        
     elif [[ -f $name ]]; then
         echo "$name is a file."
+        lines=$(cat $name | wc -l)
+        echo "No. of lines: $lines"
+        permission=$(ls -l $name | cut -d ' ' -f 1 | cut -c 2-4)
+        echo "User Permission: $permission"
+        
+        if [[ "${permission:1:1}" == "w" ]]; then
+            echo "The user has write permission."
+            echo "Modifying file permission..."
+            chmod u-w $name
+            permission=$(ls -l $name | cut -d ' ' -f 1 | cut -c 2-4)
+            echo "User Permission: $permission"
+        fi
+        
     else
         echo "$name is neither a file nor directory."
     fi
+    
+    echo
 done
