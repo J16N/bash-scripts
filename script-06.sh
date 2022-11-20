28619c9bada1367c5b982687ca8bfb922036a8a1
Tsubasa <scientistjishan@gmail.com>
2022-11-20 13:19:01 +0530

    Script to find all the palindrome words from file.

---
+++ script-06.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# script-06 : Find palindrome words from file
+# Author    : Mr. Rabbit
+
+usage()
+{
+    echo "usage: ${0##*/} <word_list_file>"
+    exit 1
+}
+
+[[ -z "$1" ]] && usage
+
+read -a words < <(cat $1 | sed 's/[.,;]//g')
+echo -e "Palindrome words are as follows: "
+
+for word in ${words[*]}; do
+    rev_word=$(rev <<< $word)
+    if [[ $rev_word == $word ]]; then echo -n "$word "; fi
+done
