419cc5b60cbb228300251aa56af1b427e5be747c
Tsubasa <scientistjishan@gmail.com>
2022-11-20 18:22:41 +0530

    Made some minor modification.

--- script-03.sh
+++ script-03.sh
@@ -17,9 +17,9 @@ echo "Get G.C.D of list of numbers."
 read -p "Enter numbers (space-seperated): " -a numbers
 numbers=($(echo ${numbers[*]} | tr ' ' '\n' | sort -n | tr '\n' ' '))
 
-ans=$(gcd ${numbers[0]} ${numbers[1]})
-for ((i = 2; i < ${#numbers[*]}; ++i)); do
-    ans=$(gcd $ans ${numbers[$i]})
+ans=${numbers[0]}
+for ((i = 1; i < ${#numbers[*]}; ++i)); do
+    ans=$(gcd $ans ${numbers[i]})
     if (( ans == 1 )); then break; fi
 done
 
